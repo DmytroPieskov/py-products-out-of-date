21b9d2e680421e4b5b8871553eaa290fc2a81dc0
Dmytro <labtech.kh@gmail.com>
2024-11-20 21:57:24 +0200

    'Solution_v1.02'

--- app/test_main.py
+++ app/test_main.py
@@ -27,13 +27,13 @@ def test_no_outdated_products():
         ]
         assert outdated_products(products) == []
 
-# def test_all_outdated_products():
-#     with patch('datetime.date') as mocked_date:
-#         mocked_date.today.return_value = datetime.date(2024, 2, 2)
-#         mocked_date.side_effect = lambda *args, **kw: datetime.date(*args, **kw)
-#
-#         products = [
-#             {"name": "salmon", "expiration_date": datetime.date(2024, 1, 10), "price": 600},
-#             {"name": "chicken", "expiration_date": datetime.date(2024, 1, 5), "price": 120}
-#         ]
-#         assert outdated_products(products) == ['salmon', 'chicken']
+def test_all_outdated_products():
+    with patch('datetime.date') as mocked_date:
+        mocked_date.today.return_value = datetime.date(2024, 2, 2)
+        mocked_date.side_effect = lambda *args, **kw: datetime.date(*args, **kw)
+
+        products = [
+            {"name": "salmon", "expiration_date": datetime.date(2024, 1, 10), "price": 600},
+            {"name": "chicken", "expiration_date": datetime.date(2024, 1, 5), "price": 120}
+        ]
+        assert outdated_products(products) == ['salmon', 'chicken']
